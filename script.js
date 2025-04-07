19b56aedcd874e96d16fa417e65b520008d2bbaa
Paul Barron <paulinobarron85@outlook.com>
2025-04-07 15:53:54 -0700

    Updates from during class

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ var bdy = window.document.querySelector("body");
 var canvas = document.getElementById("canvas");
 
 canvas.width = 1500;
-canvas.height = 900;
+canvas.height = 800;
 
 var keys = [], clicked = false, mouseX, mouseY;
 
 var inter, runner;
 
-bdy.addEventListener("keydown", function(e) {//Keyboard reading
-	keys[e.keyCode] = true;
+//bdy.addEventListener("keydown", function(e) {//Keyboard reading
+//	keys[e.keyCode] = true;
+//});
+//bdy.addEventListener("keyup", function(e) {
+//	keys[e.keyCode] = false;
+//});
+bdy.addEventListener('keydown', (event) => {
+    keys[event.key] = true;
 });
-bdy.addEventListener("keyup", function(e) {
-	keys[e.keyCode] = false;
+bdy.addEventListener('keyup', (event) => {
+    keys[event.key] = false;
 });
+
 
 bdy.addEventListener("mousedown", function() {//Mouse reading
 	clicked = true;
@@ -26,8 +33,6 @@ bdy.addEventListener("mousemove", function(e) {
 	mouseX = (e.clientX - rect.left) * scaleX;
 	mouseY = (e.clientY - rect.top) * scaleY;
 });
-
-
 
 var ctx = window.document.getElementById("canvas").getContext("2d");//2d context
 ctx.imageSmoothingEnabled = true;
@@ -44,9 +49,33 @@ var cam = {
 	y: 0,
 };//Camera stuff
 
-
-function runner() {
+function Player(x, y) {
+    this.x = x;
+    this.y = y;
+    this.w = 40;
+    this.h = 30;
+}
+Player.prototype.draw = function() {
+    ctx.fillStyle = "black"
+    ctx.rect(this.x, this.y, this.w, this.h);
+};
+Player.prototype.update = function() {
+    if(keys["ArrowLeft"]){
+        console.log("HI");
+    }
     
+};
+
+var player = new Player(300, 300)
+
+function runner() {//draw fuction
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, 1500, 800);
+
+    
+
+    player.draw();
+    player.update();
 }
 
 inter = window.setInterval(runner, 1000 / 60);
